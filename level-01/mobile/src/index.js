6901f12a720e9ff6d327bf123e4926d0795ac88a
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2020-11-15 14:31:22 -0300

    first mobile project with react-native

---
+++ level-01/mobile/src/index.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react';
+import {
+  SafeAreaView,
+  Text,
+  FlatList,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+} from 'react-native';
+
+import api from './services/api';
+
+export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then((response) => {
+      setProjects(response.data);
+    });
+  }, []);
+
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
+
+  return (
+    <>
+      <StatusBar barStyle="light-content" />
+
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={(project) => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text>Adicionar</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+  },
+
+  project: {
+    color: '#FFF',
+  },
+
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
