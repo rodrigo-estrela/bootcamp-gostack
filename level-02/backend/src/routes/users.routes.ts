b08faaf9b89da1ee7199fd5fc6ee7b3c3e85d843
Rodrigo Estrela <dev.rodrigoestrela@gmail.com>
2020-12-22 22:08:38 -0300

    added avatar upload

--- level-02/backend/src/routes/users.routes.ts
+++ level-02/backend/src/routes/users.routes.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
   try{
@@ -28,6 +32,10 @@ usersRouter.post('/', async (request, response) => {
   } catch(err) {
     return response.status(400).json({ message: err.message })
   }
+})
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+  return response.json({ ok: true })
 })
 
 export default usersRouter;
